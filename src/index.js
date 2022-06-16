bc7e4bfbeb221634c9997329860f65a7994e3d82
masum2617 <masum2617@gmail.com>
2022-06-17 01:49:30 +0600

    front end work starts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Entryfile/Main";
+window.Popper = require("popper.js").default;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<Main/>, document.getElementById('app'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (module.hot) { // enables hot module replacement if plugin is installed
+ module.hot.accept();
+}
